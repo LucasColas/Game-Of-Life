f53645e1d1569f10b08d7896189c33b31deaf14e
LucasColas <lucas.colas13@gmail.com>
2022-04-23 15:03:51 +0200

    update

--- Ressources/game.py
+++ Ressources/game.py
@@ -3,18 +3,20 @@ pygame.init()
 from .variables import Black,White
 
 class Game():
-    def __init__(self):
+    def __init__(self, n_rows, n_cols):
         self.display_surface = pygame.display.get_surface()
         self.w = self.display_surface.get_width()
         self.h = self.display_surface.get_height()
-        self.rows = self.display_surface.get_height()//20
-        self.cols = self.display_surface.get_width()//20
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.rows = self.display_surface.get_height()//self.n_rows
+        self.cols = self.display_surface.get_width()//self.n_cols
 
 
     def create_grid(self):
         self.display_surface.fill(White)
-        for row in range(20):
+        for row in range(self.n_rows):
             pygame.draw.line(self.display_surface, Black, (0,row*self.rows), (self.w, row*self.rows))
 
-        for col in range(20):
+        for col in range(self.n_cols):
             pygame.draw.line(self.display_surface, Black, (col*self.cols,0), (col*self.cols,self.h))
