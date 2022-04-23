ab1949a89e2efc07b37b84db252faa6456525b61
LucasColas <lucas.colas13@gmail.com>
2022-04-23 14:33:19 +0200

    update

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ pygame.init()
 Width, Height = 1500, 700
 Win = pygame.display.set_mode((Width, Height))
 
+clock = pygame.time.Clock()
+
 def main():
+
+    run = True
+    FPS = 60
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
+main()
