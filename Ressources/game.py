35b74f7d6437b337395d7f49c0b7003654f15d33
LucasColas <lucas.colas13@gmail.com>
2022-04-23 14:55:02 +0200

    update

--- Ressources/game.py
+++ Ressources/game.py
@@ -1,11 +1,17 @@
 import pygame
 pygame.init()
+from .variables import White
 
 class Game():
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
+        self.w = self.display_surface.get_width()
+        self.h = self.display_surface.get_height()
+        self.rows = self.display_surface.get_height()//20
+        self.cols = self.display_surface.get_width()//20
 
 
     def create_grid(self):
         self.display_surface.fill(White)
-        
+        for row in range(self.rows):
+            pygame.draw.line(self.display_surface, color, row*, end_pos)
