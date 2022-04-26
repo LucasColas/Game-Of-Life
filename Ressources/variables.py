b8a61b4e961f8a2d68dfd5bd681990f2d8b0be14
LucasColas <lucas.colas13@gmail.com>
2022-04-26 16:23:46 +0200

    update

--- Ressources/variables.py
+++ Ressources/variables.py
@@ -11,11 +11,15 @@ n_cols = 50
 grid = [[0 for i in range(n_rows)] for j in range(n_cols)]
 
 
-grid[1][0] = 1
 grid[41][40] = 1
 
-grid[0][1] = 1
+
 grid[1][2] = 1
-grid[2][0] = 1
 grid[2][1] = 1
 grid[2][1] = 1
+
+
+grid[20][4] = 1
+grid[20][5] = 1
+grid[21][4] = 1
+grid[21][5] = 1
