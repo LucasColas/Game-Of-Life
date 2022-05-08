0e4a07edde6caf9cba490fc150ff3a0c29b21719
LucasColas <lucas.colas13@gmail.com>
2022-05-08 15:33:07 +0200

    update

--- Version2/main.py
+++ Version2/main.py
@@ -18,13 +18,15 @@ def main():
     nb = ''
 
     while run:
-
+        #print(nb)
         if not nb_entered:
+            Win.fill(White)
+            print(nb)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     quit()
-                    
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_BACKSPACE:
                         nb = nb[:-1]
@@ -32,21 +34,25 @@ def main():
                     elif event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
                         nb_entered = True
                     else:
-                        nb += event.unicode
+                        print(event.unicode, type(event.unicode))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                quit()
+                        if event.unicode >= '0' and event.unicode <= '9':
+                            nb += event.unicode
+
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    quit()
 
 
 
 
 
 
-        new_Game.draw_grid(grid)
-        #time.sleep(1)
-        new_Game.update_grid(grid)
+            new_Game.draw_grid(grid)
+            #time.sleep(1)
+            new_Game.update_grid(grid)
 
         pygame.display.update()
         clock.tick(FPS)
